724d28d1865971d7544401021f6455409c3145f6
Ernest John Lanz <ejlanz.za@gmail.com>
2019-03-19 12:30:06 +0200

    2nd commit recursion functions updated

--- recandsort/recursion.py
+++ recandsort/recursion.py
@@ -1,6 +1,10 @@
 def sum_array(array):
 
     '''Return sum of all items in array'''
+    if len(array)==0:
+        return 0
+    else:
+        return array[0] + sum_array(array[1:])
 
 def fibonacci(n):
 
@@ -30,7 +34,15 @@ def fibonacci(n):
 def factorial(n):
 
     '''Return n!'''
+    if n == 1:
+        return n
+    else:
+        return n * factorial(n-1)
 
 def reverse(word):
 
     '''Return word in reverse'''
+    if len(word) == 0:
+        return word
+    else:
+        return reverse(word[1:]) + word[0]
